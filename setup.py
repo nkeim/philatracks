4bcf237f2482f7436bd729fbdb6ad542309960ef
Nathan Keim <nkeim@seas.upenn.edu>
2014-08-26 17:00:18 -0700

    Change version to 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='philatracks',
-    version='0.1',
+    version='0.2',
     author='Nathan C. Keim',
     author_email='nkeim@seas.upenn.edu',
     url='https://github.com/nkeim/philatracks',
